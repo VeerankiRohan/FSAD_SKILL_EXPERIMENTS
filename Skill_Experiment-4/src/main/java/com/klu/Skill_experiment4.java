5709998fae3b40bf968e96aa4040a3e6d07bc9a6
VEERANKI ROHAN <2400030504@kluniversity.in>
2026-03-24 07:36:39 +0530

    Add files via upload

---
+++ Skill_Experiment-4/src/main/java/com/klu/Skill_experiment4.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skill_experiment4 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Skill_experiment4.class, args);
+	}
+
+}
