791bff27613aed2fd5144dcf371977d1fc2d689e
VEERANKI ROHAN <2400030504@kluniversity.in>
2026-03-24 07:39:55 +0530

    Add files via upload

---
+++ Skill_Experiment-5/src/test/java/com/klu/Skill_experiment5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skill_experiment5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
